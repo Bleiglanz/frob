44ab689c824d8ed3f0c677e2931e5aec03522645
Anton Rechenauer <antonrechenauer@gmail.com>
2020-10-18 20:52:38 +0200

    t4 auswertung fertig

--- src/bin/t4neu.rs
+++ src/bin/t4neu.rs
@@ -5,70 +5,62 @@ use clap::{Arg, App};
 use frob::modules::fast_semigroup::{Fast, fast};
 use frob::modules::Semigroup;
 use std::io::Write;
-use frob::modules::wilf::{WilfSet, generatewilf};
 
-
-fn prepare_generators(modul: usize, residue: usize, allgen:&[usize]) -> Vec<usize>{
-    allgen.iter().filter(|x| { residue == *x % modul }).map(|x|{*x}).collect()
+fn t4list(max:usize)->Vec<usize>{
+    let mut res:Vec<usize> = Vec::with_capacity(20000);
+    let mut zaehler = 0;
+    for t in 1..u64::max_value() as u64 {
+        if primal::is_prime(1+4*t) && primal::is_prime(3+4*t) && primal::is_prime(1+6*t) {
+            res.push(t as usize);
+            zaehler+=1;
+        }
+        if zaehler>=max { break; }
+    }
+    res
 }
 
-fn frobenius(modul: usize, residue: usize, start: usize, stop: usize) {
+
+fn frobenius(ts:Vec<usize>) {
 
     let raw:Vec<usize>= primal::Primes::all().take(8000000).collect();
-    let full = prepare_generators(modul,residue,&raw);
-    let mut out = std::fs::File::create(format!("./schrittw_mod{}residue{},p_n{}to{}.csv", modul, residue, start, stop)).expect("Unable to create file");
-    let head = "modul; resi;begin_slice;end_slice; p_n;   p_n+k;    m(S);    e(S);  #(S<F);    f(S);f(S)-3m(S);   stable;    u(S); fne(S);   sum(A)\n";
+    let mut out = std::fs::File::create(format!("./schrittw_t4_{}.csv",ts.len())).expect("Unable to create file");
+    let head = "      t; erste;   letzte;    m(S);     e(S);    f(S); f(S)/m(S) ; size  \n";
     out.write_all(head.as_bytes()).expect("head?");
     print!("{}", head);
 
+    for t in ts {
 
-    for begin_slice in start..stop {
-
-        let mut end_slice=begin_slice+10;
-
-        loop {
-
-            let gens: &[usize] = &full[begin_slice..end_slice];
-
-            if gens.len() < 3 { continue; };
-
-            let res2: Fast = { fast(&gens) };
-
-            let saturated:bool = res2.f() + 1 <= full[end_slice];
-
-            let max_even_gap = res2.max_even_gap();
-
-            let g_n = res2.count_gap;
-
-            let d_min = (2*res2.sum_apery() / (res2.m())) - (res2.f() + res2.m());
-
-            //let pi3p = raw.iter().filter(|x|{**x<=(3*res2.m())}).count();
-            assert_eq!(res2.f()+res2.m(),res2.max_apery(),"max apery ist fn+p");
-            assert_eq!(0, res2.f()+d_min +1 - 2*g_n);
-            assert_eq!(d_min,res2.count_gap - res2.count_set);
-            let ausgabe = format!("{:5};{:5};{:8};{:8};{:8};{:8};{:8};{:8};{:8};{:8};{:10};{};{:8};{:8};{:8}\n",
-                                  modul, residue,
-                                  begin_slice+2, end_slice,
-                                  full[begin_slice], full[end_slice - 1],
-                                  res2.m(), res2.e(),
-                                  res2.count_set, res2.f(), res2.f() as i64 -3*res2.m() as i64,
-                                  if saturated { "saturated" } else { "         " },
-                                  res2.u, max_even_gap,
-                                  res2.sum_a);
-
-            print!("{}", ausgabe);
-            //let ws:WilfSet = generatewilf(&gens);
-            //println!("Wilf-e {} neu-e {}",ws.e,res2.e());
-            //let html_title = &format!("./{}_Primzahl_{}.html",begin_slice+2,res2.m());
-            //let mut outhtml = std::fs::File::create(html_title).expect("Unable to create file");
-            //outhtml.write_all(ws.to_html(html_title).as_bytes()).expect("html?");
-            if res2.f_over_m() < 20f64 {
-                out.write_all(ausgabe.as_bytes()).expect("ausgabe??");
+        let mut full:Vec<usize> = Vec::with_capacity(20000);
+        let mut i = 0;
+        let first = 4*t + 1;
+        let mut last : usize =0;
+        loop{
+            let p = raw[i];
+            if p>=4*t+1 && 2*p<=3*(4*t+1) {
+                full.push(p);
             }
-            if saturated {
-                break;
-            }
-            end_slice += 1 ;
+            if 2*p<=3*(4*t+1) && 2*raw[i+1]>3*(4*t+1) { last = p; }
+            if 2*p>3*(4*t+1) { break; }
+            i=i+1;
+        }
+        let gens:&[usize] = &full;
+        if gens.len() < 2 { continue; };
+        let res2: Fast = { fast(&gens) };
+        let max_even_gap = res2.max_even_gap();
+        let g_n = res2.count_gap;
+        let d_min = (2*res2.sum_apery() / (res2.m())) - (res2.f() + res2.m());
+        assert_eq!(res2.f()+res2.m(),res2.max_apery(),"max apery ist fn+p");
+        assert_eq!(0, res2.f()+d_min +1 - 2*g_n);
+        assert_eq!(d_min,res2.count_gap - res2.count_set);
+        let ausgabe = format!("{:8};{:8};{:8};{:8};{:8};{:8};{};{:?}\n",
+                              t,
+                              first, last,
+                              res2.m(), res2.e(),
+                              res2.f(), res2.f() as f64 / res2.m() as f64, full.len());
+
+        print!("{}", ausgabe);
+        if res2.f_over_m() < 20f64 {
+            out.write_all(ausgabe.as_bytes()).expect("ausgabe??");
         }
     }
 }
@@ -78,21 +70,6 @@ fn main() {
         .version("0.0")
         .author("Anton Rechenauer")
         .about("compute frobenius")
-        .arg(Arg::with_name("modul")
-            .help("the modulus, in which arithmetic progression to search")
-            .required(true)
-            .default_value("2")
-        )
-        .arg(Arg::with_name("residue")
-            .help("the residue, consider only primes congruent this mod modul")
-            .required(true)
-            .default_value("1")
-        )
-        .arg(Arg::with_name("start")
-            .help("where to begin, a n th prime")
-            .required(true)
-            .default_value("1")
-        )
         .arg(Arg::with_name("stop")
             .help("where to stop, a n th prime")
             .required(true)
@@ -100,11 +77,8 @@ fn main() {
         )
         .get_matches();
 
-    let modul: usize = matches.value_of("modul").unwrap().parse().unwrap();
-    let residue: usize = matches.value_of("residue").unwrap().parse().unwrap();
-
-    let start: usize = matches.value_of("start").unwrap().parse().unwrap();
     let stop: usize = matches.value_of("stop").unwrap().parse().unwrap();
-
-    frobenius(modul, residue, start, stop);
+    let ts = t4list(stop);
+    println!("t4 list bis {} ist {:?}",stop,ts);
+    frobenius( ts );
 }
